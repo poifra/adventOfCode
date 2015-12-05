26014bcb639ee297880d3630b988826ba11feb4d
poifra <fpoitras123@gmail.com>
2015-12-05 14:29:14 -0500

    first

---
+++ day4.py
@@ -0,0 +1,23 @@
+def magicNumber(input):
+	from hashlib import md5
+	for i in range(10000000):
+		if(md5((input+str(i)).encode()).hexdigest()[:5] == "00000"):
+			return i
+			
+def magicNumber6(input):
+	from hashlib import md5
+	for i in range(10000000):
+		if(md5((input+str(i)).encode()).hexdigest()[:6] == "000000"):
+			return i
+
+if(__name__=="__main__"):
+	input = "bgvyzdsv"
+	def part1():
+		assert (magicNumber("abcdef") == 609043)
+		assert (magicNumber("pqrstuv") == 1048970)
+		return magicNumber(input)
+	def part2():
+		return magicNumber6(input)
+		
+	print("part1",part1())
+	print("part2",part2())
