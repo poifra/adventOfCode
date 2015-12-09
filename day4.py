937d3f8e8da34efd8afd8f1546087a1fb77a9830
poifra <fpoitras123@gmail.com>
2015-12-09 10:29:02 -0500

    day 7 solution

--- day4.py
+++ day4.py
@@ -1,24 +1,9 @@
-def magicNumber(input):
-#bruteforce ftw
+def magicNumber(input,n):
 	from hashlib import md5
-	for i in range(10000000):
-		if(md5((input+str(i)).encode()).hexdigest()[:5] == "00000"):
-			return i
-			
-def magicNumber6(input):
-	from hashlib import md5
-	for i in range(10000000):
-		if(md5((input+str(i)).encode()).hexdigest()[:6] == "000000"):
-			return i
+	for i in range(10000000): 
+		if(md5((input+str(i)).encode()).hexdigest()[:n] == '0'*n): return i
 
 if(__name__=="__main__"):
 	input = "bgvyzdsv"
-	def part1():
-		assert (magicNumber("abcdef") == 609043)
-		assert (magicNumber("pqrstuv") == 1048970)
-		return magicNumber(input)
-	def part2():
-		return magicNumber6(input)
-		
-	print("part1",part1())
-	print("part2",part2())
+	print(magicNumber(input,5))
+	print(magicNumber(input,6))
