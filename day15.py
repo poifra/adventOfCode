57edb691d37a43046650ceee24e85b8d5948e5c2
poifra <fpoitras123@gmail.com>
2015-12-22 22:32:58 -0500

    more catching up

--- day15.py
+++ day15.py
@@ -1,23 +1,36 @@
 import sys
 
-def bestCombo(ingredients):
-	#brute force that shit
-	maxValue = -100000000000
+def bestCombo(ing, calCount = False):
+	'''
+	brute force that shit
+	'''
+	maxValue = -1
 	for i in range(100):
-		for j in range(100):
-			for k in range(100):
-				for l in range(100):
-					if i+j+k+l <= 100:
-						score = 0
-						for ing in ingredients:
-						if score > maxValue:
-							maxValue = score
-	return score
+		for j in range(100-i):
+			for k in range(100-i-j):
+			
+				l = 100-i-j-k #all that remains, no need a loop
+				totalCap = ing[0]['capacity']*i+j*ing[1]['capacity']+k*ing[2]['capacity']+l*ing[3]['capacity']
+				totalDur = ing[0]['durability']*i+j*ing[1]['durability']+k*ing[2]['durability']+l*ing[3]['durability']
+				totalFla = ing[0]['flavor']*i+j*ing[1]['flavor']+k*ing[2]['flavor']+l*ing[3]['flavor']
+				totalTex = ing[0]['texture']*i+j*ing[1]['texture']+k*ing[2]['texture']+l*ing[3]['texture']
+				totalCal = ing[0]['calories']*i+j*ing[1]['calories']+k*ing[2]['calories']+l*ing[3]['calories']
+				
+				if totalCap <= 0 or totalDur <= 0 or totalFla <= 0 or totalTex <=0:
+					score = 0
+				else:
+					score = totalCap*totalDur*totalFla*totalTex
+
+				if calCount and totalCal != 500:
+					continue
+				if score > maxValue:
+					maxValue = score
+	return maxValue
 
 if __name__ == '__main__':
 	with open(sys.argv[1], 'r') as file:
 		content = file.read().replace(',','').replace(':','').split('\n')
-		ingredients = []
+		ings = []
 		for line in content:
 			name,_,capacity,_,durability,_,flavor,_,texture,_,calories = line.split(' ')
 			ing = dict()
@@ -27,5 +40,6 @@ if __name__ == '__main__':
 			ing['flavor'] = int(flavor)
 			ing['texture'] = int(texture)
 			ing['calories'] = int(calories)
-			ingredients.append(ing)
-		print(bestCombo(ingredients)
+			ings.append(ing)
+		print("part1", bestCombo(ings))
+		print("part2", bestCombo(ings,True))
