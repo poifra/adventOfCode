b6980f7da016bc6035ca4a8487f20c607651fdc3
Francois <fpoitras123@gmail.com>
2016-12-10 12:14:46 -0500

    removed old files

--- day4.py
+++
@@ -1,9 +0,0 @@
-def magicNumber(input,n):
-	from hashlib import md5
-	for i in range(10000000): 
-		if(md5((input+str(i)).encode()).hexdigest()[:n] == '0'*n): return i
-
-if(__name__=="__main__"):
-	input = "bgvyzdsv"
-	print(magicNumber(input,5))
-	print(magicNumber(input,6))
