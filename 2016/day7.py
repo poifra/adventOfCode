54428d39b033de15ff20f48f9391a5c5d6f389e3
Francois <fpoitras123@gmail.com>
2016-12-11 09:55:24 -0500

    more solved and misc fixes

--- 2016/day7.py
+++ 2016/day7.py
@@ -1,20 +1,32 @@
+import itertools
+import re
+
 def has_abba(word):
-	return any(a==d and b==c and a!=b for a,b,c,d in zip(x, x[1:], x[2:], x[3:]))
+	return any(a==d and b==c and a!=b for a,b,c,d in zip(word, word[1:], word[2:], word[3:]))
 
 def day7part1():
 	with open('day7data.txt') as f:
 		data = f.read().split('\n')
 		valid = 0
-		toggle = itertools.cycle[False,True].__next__
 		for line in data:
+			line = line.replace('[',']').split(']')
+			toggle = itertools.cycle([False,True]).__next__
 			form = [(toggle(),line[i]) for i,x in enumerate(line)]
 			good = True
+			found = False
 			for block in form:
-				if has_abba(block[1]) and block[0]:
-					good = False
+				if not good:
 					break
-			valid += good
+				if has_abba(block[1]):
+					if block[0]:
+						good = False
+					else:
+						found = True
+			valid += (good and found)
 		return valid
 
 def day7part2():
-	pass
+	#shamelessly stolen from reddit because fuck regexes
+	lines = [re.split(r'\[([^\]]+)\]', line) for line in open('day7data.txt')]
+	parts = [(' '.join(p[::2]), ' '.join(p[1::2])) for p in lines]
+	print('Answer #2:', sum(any(a == c and a != b and b+a+b in hn for a, b, c in zip(sn, sn[1:], sn[2:])) for sn, hn in parts))
