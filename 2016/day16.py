138bda6e9f4683e26ef1a14cad541b23a4884392
Francois <fpoitras123@gmail.com>
2016-12-17 15:52:08 -0500

    optimization to day16

--- 2016/day16.py
+++ 2016/day16.py
@@ -1,31 +1,27 @@
-def convert(a):
-	b = a[::-1] #reverse
-	b = b.replace('0', '2').replace('1', '0').replace('2', '1')
-	return a + '0' + b
-def part1(size = 272):
-	data = '10111011111001111'
+def nextCheck(chr_iter):
+	for c in chr_iter:
+		yield '1' if c == next(chr_iter) else '0'
+
+def part1(data='10111011111001111', size = 272):
 	#data = '10000'
 	#size = 20
 	a = data
 	while len(a) < size:
-	#	print(a,"is still too short len =",len(a))
-		a = convert(a)
-	#print(a, 'is long enough len =',len(a))
-	a = a[:size] #keep only the data we need
-	#print("The first",size,"chars are", a)
+		a += '0'+a[::-1].replace('0', '2').replace('1', '0').replace('2', '1')
+	a = a[:size]
 	while len(a) % 2 == 0:
-		a = ''.join(['1' if b[0] == b[1] else '0' for b in [a[i:i+2] for i  in range(0,len(a),2)]])
-		#print('checksum is now',a,"of length",len(a))
+		a = ''.join(nextCheck(iter(a)))
 	return a
 
 def part2():
-	return part1(35651584)
-
+	return part1(size=35651584)
+ 
 if __name__ == '__main__':
 	import time
+	assert part1('10000',20) == '01100'
 	start = time.time()
-	part1()
+	print(part1())
 	print("time for part 1 :",time.time()-start)
 	start = time.time()
-	part2()
+	print(part2())
 	print("time for part2 :",time.time()-start)
