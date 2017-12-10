29291868ed2f7aaaae73e6014398a695c63ad775
Francois <fpoitras123@gmail.com>
2017-12-09 22:39:57 -0500

    jk there's only day 6 in there

---
+++ 2017/day6.py
@@ -0,0 +1,27 @@
+
+def problem():
+	# problem description : http://adventofcode.com/2017/day/6
+	memory = [11,11,13,7,0,15,5,5,4,4,1,1,7,1,15,11]
+	#memory = [0,2,7,0] #test input
+	size = len(memory)
+	seen = []
+	
+	#part 1
+	loop = False
+	while not memory in seen:
+		seen.append(memory[:])
+		top = max(memory)
+		i = memory.index(top)
+		memory[i] = 0
+		while top:
+			top -= 1
+			memory[(i+1) % size] += 1
+			i += 1
+		#print(memory)
+
+	print("Part 1",len(seen))
+	#part 2
+	print("Part 2",len(seen) - seen.index(memory))
+
+if __name__ == '__main__':
+	problem()
